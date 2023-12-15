639c9b3788b5f517b78edbc7555e6870fed81514
Maher Kefi <kefi.maher@esprit.tn>
2023-12-15 07:57:56 +0100

    15/12/2023 07:57

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { product } from '../core/models/product';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+  price!:number;
+listProducts:product[]=[
+  {id:1,title:'t-shirt 1',price:50,quantity:10,like:0,description:"test"},
+  {id:2,title:'t-shirt 2',price:30,quantity:5,like:0,description:""},
+  {id:3,title:'t-shirt 3',price:20,quantity:0,like:5,description:""}
+]
 
+increment(i:number){
+this.listProducts[i].like++;
+}
+buy(i:number){
+  this.listProducts[i].quantity--;
+
+}
 }
